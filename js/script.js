1147cfec49c2d99278d27728b896f9eed1d2c5e7
sphjlin <sjl81@pitt.edu>
2025-04-06 23:48:29 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 // script.js
-// This is the JavaScript file for the client project
 // This file contains the JavaScript code for the client project
 document.getElementById("year").innerHTML = new Date().getFullYear();
+// this javascript code is from Codex world:https://www.codexworld.com/how-to/get-current-year-to-display-dynamic-year-in-copyright-using-javascript/
