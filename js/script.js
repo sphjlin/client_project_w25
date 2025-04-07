a7ca3e6acf415b9da55599bf7fdc6b3ed826e616
sphjlin <sjl81@pitt.edu>
2025-04-06 23:04:34 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// script.js
+// This is the JavaScript file for the client project
+// This file contains the JavaScript code for the client project
+document.getElementById("year").innerHTML = new Date().getFullYear();
